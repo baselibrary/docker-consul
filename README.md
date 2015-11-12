c2c7679f9a9766d985285daa28f415f4958d9d81
Shawn Ma <qsma@thoughtworks.com>
2015-11-12 09:32:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## ThoughtWorks Docker Image: consul
+
+[![](http://dockeri.co/image/baselibrary/consul)](https://registry.hub.docker.com/u/baselibrary/consul/)
+
+### Base Docker Image
+
+* `latest`: consul 0.5
+* `0.5`   : consul 0.5
+
+
+### Installation
+
+    docker pull baselibrary/consul
+
+### Usage
+
+    docker run -it --rm baselibrary/consul
---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+
+for version in "${versions[@]}"; do	
+  fullVersion="$(git ls-remote --tags https://github.com/hashicorp/consul.git | cut -d$'\t' -f2 | grep -E '^refs/tags/v'"${version}"'.[0-9]$' | cut -dv -f2 | sort -rV | head -n1 )"
+  echo $fullVersion
+  (
+		set -x
+		sed '
+			s/%%CONSUL_MAJOR%%/'"$version"'/g;
+			s/%%CONSUL_RELEASE%%/'"$fullVersion"'/g;
+		' Dockerfile.template > "$version/Dockerfile"
+	)
+done
