0550cb76240f4478e5e080f68591f5e5eebf2b4a
Shawn Ma <qsma@thoughtworks.com>
2016-05-20 15:37:49 +0800

    update

--- 0.6/docker-entrypoint.sh
+++ 0.6/docker-entrypoint.sh
@@ -10,17 +10,14 @@ VPN_TYPE=${VPN_TYPE:=openvpn}
 #run sslvpn in background
 if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
 	## check the required parameters
-	if [ -z "$VPN_TYPE" -o -z "$VPN_HOST" -o -z "$VPN_PORT" -o -z "$VPN_USER" ]; then
-		echo >&2 'Error: vpn option is not specified'
-		exit 1
-	fi
+	
   ##### run scripts  #####
   echo "========================================================================"
   echo "startup: run expect                                                     "
   echo "========================================================================"
   confd -backend=rancher --prefix=/2015-07-25 
  
-  exec consul agent -config-file=/etc/consul/config/server.json
+  exec consul agent -config-file=/etc/consul/server.json
 else
   exec "$@"
 fi
